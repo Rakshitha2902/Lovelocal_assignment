c582609c1172c92f90558f6c3aef5d35cee496dc
Rakshitha2902 <122611225+Rakshitha2902@users.noreply.github.com>
2023-12-07 14:02:42 +0530

    Add files via upload

---
+++ Medium_2_solution.py
@@ -0,0 +1,42 @@
+def majority_elements(nums):
+    if not nums:
+        return []
+
+    candidate1, count1 = None, 0
+    candidate2, count2 = None, 0
+
+    for num in nums:
+        if num == candidate1:
+            count1 += 1
+        elif num == candidate2:
+            count2 += 1
+        elif count1 == 0:
+            candidate1, count1 = num, 1
+        elif count2 == 0:
+            candidate2, count2 = num, 1
+        else:
+            count1 -= 1
+            count2 -= 1
+
+    count1, count2 = 0, 0
+    for num in nums:
+        if num == candidate1:
+            count1 += 1
+        elif num == candidate2:
+            count2 += 1
+
+    result = []
+    if count1 > len(nums) // 3:
+        result.append(candidate1)
+    if count2 > len(nums) // 3:
+        result.append(candidate2)
+
+    return result
+
+
+nums_str = input()
+nums = list(map(int, nums_str.split()))
+
+
+result = majority_elements(nums)
+print(result)
